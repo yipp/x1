6d5c300764092c3d7f641fa0f63df124875bcded
ppdashi <1030681978@qq.com>
2017-09-09 15:38:21 +0800

    完善时间工具类

--- src/main/java/org/x1/calendar/DateUtils.java
+++ src/main/java/org/x1/calendar/DateUtils.java
@@ -1,5 +1,6 @@
 package org.x1.calendar;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
@@ -36,5 +37,72 @@ public class DateUtils {
     }
     public static long dayEndMillis(){
         return dayEndCalendar().getTimeInMillis();
+    }
+    /*
+    *当前天+7天
+     */
+    private static Calendar weekEndCalendar(){
+        Calendar c = Calendar.getInstance();
+        c.set(Calendar.DAY_OF_YEAR,c.get(Calendar.DAY_OF_YEAR)+7);
+        return c;
+    }
+    public static int week(){
+        Date date = weekEndCalendar().getTime();
+        SimpleDateFormat format = new SimpleDateFormat("yyyyMMdd");
+        String from = format.format(date);
+        int to = Integer.parseInt(from);
+        return to;
+    }
+    public static long weekMillis(){
+        return weekEndCalendar().getTimeInMillis();
+    }
+    private static Calendar monthEndCalendar(){
+        Calendar c = Calendar.getInstance();
+        c.set(c.get(Calendar.YEAR),c.get(Calendar.MONDAY),c.get(Calendar.DAY_OF_MONTH),0,0,0);
+        c.set(Calendar.DAY_OF_MONTH,c.getActualMaximum(Calendar.DAY_OF_MONTH));
+        c.set(Calendar.HOUR_OF_DAY,20);
+        return  c;
+    }
+    public static int month(){
+        Date date = monthEndCalendar().getTime();
+        SimpleDateFormat format = new SimpleDateFormat("yyyyMMdd");
+        String from = format.format(date);
+        int to = Integer.parseInt(from);
+        return to;
+    }
+    public static long monthEndMllis(){
+        return monthEndCalendar().getTimeInMillis();
+    }
+    public static int getTime(String date){
+        int time = 0;
+        try {
+            SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+            SimpleDateFormat to = new SimpleDateFormat("yyyyMMdd");
+            Date date1 = format.parse(date);
+            String timeStr = to.format(date1);
+            time = Integer.parseInt(timeStr);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return time;
+    }
+    public static long getTimeMillis(String date){
+        long time = 0;
+        try {
+            time = org.apache.commons.lang3.time.DateUtils.parseDate(date,"yyyy-MM-dd").getTime();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return time;
+    }
+
+    /**
+     * 30秒之后
+     * @return
+     */
+    public static long getFutureTimeMillis(){
+        Calendar c=Calendar.getInstance();
+        c.add(Calendar.SECOND,30);
+        return c.getTimeInMillis();
     }
 }
